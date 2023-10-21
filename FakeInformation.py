a69763ea4eb4a9561f95aba40d94346eaf832b1d
AmirRamezanzadeh <142434947+Amirrmezani@users.noreply.github.com>
2023-10-21 21:45:01 +0330

    python

---
+++ FakeInformation.py
@@ -0,0 +1,9 @@
+# Fake personall information
+
+# Firt write on Terminal pip install faker than =>
+from faker import Faker
+fake = Faker()
+print(fake.name())
+print(fake.email())
+print(fake.country())
+print(fake.profile())
